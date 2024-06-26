cd886c7caee5a5df4479b72900ce6701c895dca8
Jeff Moore <2699839+pbs-jmo@users.noreply.github.com>
2024-06-26 16:51:48 -0400

    A little cleaner

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,10 +3,10 @@
 rm -rf public/lib || exit
 mkdir -p public/lib || exit
 
-cp node_modules/videojs-contrib-eme/dist/videojs-contrib-eme.min.js   public/lib/videojs-contrib-eme.min.js || exit
-cp node_modules/videojs-contrib-dash/dist/videojs-dash.min.js         public/lib/videojs-dash.min.js || exit
+cp node_modules/videojs-contrib-eme/dist/videojs-contrib-eme.min.js   public/lib || exit 1
+cp node_modules/videojs-contrib-dash/dist/videojs-dash.min.js         public/lib || exit 1
 
-cp node_modules/video.js/dist/video.min.js                            public/lib/video.min.js || exit
-cp node_modules/video.js/dist/video-js.min.css                        public/lib/video-js.min.css || exit
+cp node_modules/video.js/dist/video.min.js                            public/lib || exit 1
+cp node_modules/video.js/dist/video-js.min.css                        public/lib || exit 1
 
 node scripts/asset-hashes.js
