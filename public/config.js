19d5dce96f0b28b3127fdd28f3ddc61f316b9853
Jeff Moore <2699839+pbs-jmo@users.noreply.github.com>
2024-06-26 17:07:47 -0400

    Boolean as intended

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 const isSafari = videojs.browser.IS_ANY_SAFARI;
 const drmEnabled = !window.location.search.match(/\bdrmoff=true\b/);
-const dashEnabled = !isSafari && drmEnabled && window.location.search.match(/\bdashon=true\b/);
+const dashEnabled = !isSafari && drmEnabled && !!window.location.search.match(/\bdashon=true\b/);
 
 export {
     drmEnabled,
